2ddb1c8f53fc9f2dc483aa710af3d58ecb6bd6e0
bai <v@gor.io>
2026-03-04 19:27:55 +0200

    chore(test): simplify vitest config and cache ignore

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,16 +4,9 @@ import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./src"),
-    },
-  },
   test: {
-    dir: "./",
     reporters: ["verbose"],
     globals: false,
-    testTimeout: 50000,
     env: loadEnv("", process.cwd(), ""),
   },
 });
